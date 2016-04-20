fe75e84607f37861e732487bdaee6f516131cc66
gromitsend <gromitsend@gmail.com>
2016-04-20 17:08:46 -0400

    Update contact.js

--- modules/contact.js
+++ modules/contact.js
@@ -9,7 +9,7 @@ function execute(req, res) {
         return;
     }
 
-    var q = "SELECT Id, Name, Phone, MobilePhone, Email FROM Contact WHERE Name LIKE '%" + req.body.text + "%' LIMIT 5";
+    var q = "SELECT Id, Name, All_Phone_Numbers__c, Phone, MobilePhone, Email FROM Contact WHERE Name LIKE '%" + req.body.text + "%' LIMIT 5";
     org.query({query: q}, function(err, resp) {
         if (err) {
             console.error(err);
@@ -22,8 +22,7 @@ function execute(req, res) {
             contacts.forEach(function(contact) {
                 var fields = [];
                 fields.push({title: "Name", value: contact.get("Name"), short:true});
-                fields.push({title: "Phone", value: contact.get("Phone"), short:true});
-                fields.push({title: "Mobile", value: contact.get("MobilePhone"), short:true});
+                fields.push({title: "Phone", value: contact.get("All_Phone_Numbers__c"), short:true});
                 fields.push({title: "Email", value: contact.get("Email"), short:true});
                 attachments.push({color: "#009cdb", fields: fields});
             });
